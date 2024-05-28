36d39dc89d51325a30dbfca5c9c389b580b0f11f
Justin Willoughby <willoughby0114@gmail.com>
2024-05-28 10:07:22 -0500

    Fix overflow layout on mobile.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { SubmitCard } from "@/components/submit-card";
 export default function Home() {
   return (
     <main className="min-h-full min-w-full flex justify-center items-center bg-white space-x-2">
-      <div className="flex flex-col space-y-4">
+      <div className="min-h-full min-w-full flex flex-col space-y-4 p-4">
         <div className="text-4xl mb-4">Hi, Justin</div>
         <div className="flex flex-col lg:flex-row xl:flex-row space-y-4 lg:space-y-0 xl:space-y-0 lg:space-x-4 xl:space-x-4">
           <StreakCard />
