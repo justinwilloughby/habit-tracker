0469798ecaf1bb1aa7e9d226098dba07bd5566e8
Justin Willoughby <willoughby0114@gmail.com>
2024-06-12 10:59:55 -0500

    Fix desktop layout after last change for mobile layout.

--- components/streak-card.tsx
+++ components/streak-card.tsx
@@ -6,7 +6,7 @@ export const StreakCard = async () => {
   const streak = await fetchCurrentStreak();
 
   return (
-    <div className="flex flex-row space-x-2 items-center border shadow-md rounded-md p-4">
+    <div className="flex flex-row space-x-2 items-center justify-center border shadow-md rounded-md p-4">
       <Image src="./flame.svg" alt="flame" height="60" width="60" />
       <div className="flex flex-col">
         <div className="text-xl">{streak ? streak.streak_length : 0}</div>
